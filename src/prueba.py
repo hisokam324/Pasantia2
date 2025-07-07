cec07548311a7632cbf553f4f1c8b86fa1c8843d
Rinaldo Almada <maurialmadab@gmail.com>
2025-07-07 19:06:12 -0300

    .

---
+++ src/prueba.py
@@ -0,0 +1,6 @@
+def get_empty():
+    """
+        Esta funcion no pide nada y devuelve una lista vacia
+    """
+    out = []
+    return out
---
+++ src2/prueba2.py
@@ -0,0 +1,6 @@
+def duplicate(input):
+    """
+        Esta funcion pide un input y lo multiplica por 2
+    """
+    out = input*2
+    return out
