3ad2568bc05d5fd755584abb0c873dbebb5c9365
Rinaldo Almada <maurialmadab@gmail.com>
2025-07-07 21:42:37 -0300

    .

--- src/prueba.py
+++ src/prueba.py
@@ -1,17 +1,17 @@
-'''
+"""
 Se implementan dos funciones basicas
-'''
+"""
 
 def get_empty():
-    '''
+    """
     Esta funcion no pide nada y devuelve una lista vacia
-    '''
+    """
     out = []
     return out
 
 def triplicate(input):
-    '''
+    """
     Esta funcion pide un input y lo multiplica por 3
-    '''
+    """
     out = input*3
     return out
