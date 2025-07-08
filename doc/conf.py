489d8cc3a32e6f79c814728d1dc252a0fcd2b682
Rinaldo Almada <maurialmadab@gmail.com>
2025-07-08 12:02:22 -0300

    .

--- doc/conf.py
+++ doc/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../src')) #importante direccion a carpeta que se quiere comentar
 
 project = 'Prueba sphinx'
 copyright = '2025, Mauricio Almada'
